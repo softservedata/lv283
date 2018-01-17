521fb90bda4dae7a1a3015ff5d93e575e73351ea
tetianahusher <tetiana.husher@gmail.com>
2018-01-17 19:20:42 +0200

    search tests are updated

--- OpenCart/SearchTests.cs
+++ OpenCart/SearchTests.cs
@@ -35,10 +35,10 @@ namespace OpenCartTest
         {
         }
 
-        private static string[] searchData = new string[] { "mac", "samsung", "5d" };
+        private static string[] positiveSearchData = new string[] { "mac", "5d", "30" };
 
-        [Test, TestCaseSource("searchData")]
-        public void SearchTest(string searchName)
+        [Test, TestCaseSource("positiveSearchData")]
+        public void PositiveSearchTest(string searchName)
         {
             driver.FindElement(By.Name("search")).SendKeys(searchName);
             driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
@@ -49,6 +49,36 @@ namespace OpenCartTest
             {
                 Assert.IsTrue(el.Text.ToUpper().Contains(searchName.ToUpper()));
             }
+        }
+
+        private static string[] negativeSearchData = new string[] { "$", " ", "" };
+
+        [Test, TestCaseSource("negativeSearchData")]
+        public void NegativeSearchTest(string searchName)
+        {
+            driver.FindElement(By.Name("search")).SendKeys(searchName);
+            driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
+
+            Assert.IsTrue(driver.FindElement(By.XPath("//div[@id='content']/p[contains(text(), 'no product')]")).Text.Contains("no product"));
+            
+            //IList<IWebElement> elements = driver.FindElements(By.XPath("//div[@class='caption']//h4//a[contains(@href, '&search=')]"));
+            //Assert.AreEqual(0, elements.Count);
+
+        }
+
+        [Test]
+        public void SearchExTaxItemsTes()
+        {
+            driver.FindElement(By.Name("search")).SendKeys("%");
+            driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
+
+            IList<IWebElement> elements = driver.FindElements(By.CssSelector(".price-tax"));
+
+            foreach (IWebElement el in elements)
+            {
+                Assert.IsTrue(el.Text.Contains("Ex Tax:"));
+            }
+
         }
     }
 }
