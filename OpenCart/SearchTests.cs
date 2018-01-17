186aaadda94becc1c54e4072505fc0402031803f
tetianahusher <tetiana.husher@gmail.com>
2018-01-17 17:52:58 +0200

    Branch init

---
+++ OpenCart/SearchTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCartTest
+{
+    [TestFixture]
+    public class SearchTests
+    {
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl("http://283-taqc.ml/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        private static string[] searchData = new string[] { "mac", "samsung", "5d" };
+
+        [Test, TestCaseSource("searchData")]
+        public void SearchTest(string searchName)
+        {
+            driver.FindElement(By.Name("search")).SendKeys(searchName);
+            driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
+
+            IList<IWebElement> elements = driver.FindElements(By.XPath("//div[@class='caption']//h4//a[contains(@href, '&search=')]"));
+
+            foreach (IWebElement el in elements)
+            {
+                Assert.IsTrue(el.Text.ToUpper().Contains(searchName.ToUpper()));
+            }
+        }
+    }
+}
