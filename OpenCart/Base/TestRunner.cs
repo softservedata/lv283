c6320bd5fd78d7b38119b3c0d334f64a490f05b1
tetianahusher <tetiana.husher@gmail.com>
2018-01-18 21:34:41 +0200

    Added new method TooLongSearchTextTest Code cleanup

---
+++ OpenCart/Base/TestRunner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCart.Base
+{
+    public abstract class TestRunner
+    {
+        protected IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl("http://283-taqc.ml/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
--- OpenCart/SearchTests.cs
+++ OpenCart/SearchTests.cs
@@ -2,40 +2,15 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using OpenCart.Base;
 
-namespace OpenCartTest
+namespace OpenCart
 {
     [TestFixture]
-    public class SearchTests
+    public class SearchTests : TestRunner
     {
-        private IWebDriver driver;
-
-        [OneTimeSetUp]
-        public void BeforeAllMethods()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-        }
-
-        [OneTimeTearDown]
-        public void AfterAllMethods()
-        {
-            driver.Quit();
-        }
-
-        [SetUp]
-        public void SetUp()
-        {
-            driver.Navigate().GoToUrl("http://283-taqc.ml/");
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-        }
-
         private static string[] positiveSearchData = new string[] { "mac", "5d", "30" };
+        private static string[] negativeSearchData = new string[] { "$", "%", " ", "" };
 
         [Test, TestCaseSource("positiveSearchData")]
         public void PositiveSearchTest(string searchName)
@@ -51,43 +26,23 @@ namespace OpenCartTest
             }
         }
 
-        private static string[] negativeSearchData = new string[] { "$", " ", "" };
-
         [Test, TestCaseSource("negativeSearchData")]
         public void NegativeSearchTest(string searchName)
         {
             driver.FindElement(By.Name("search")).SendKeys(searchName);
             driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
 
-            Assert.IsTrue(driver.FindElement(By.XPath("//div[@id='content']/p[contains(text(), 'no product')]")).Text.Contains("no product"));
-            
-            //IList<IWebElement> elements = driver.FindElements(By.XPath("//div[@class='caption']//h4//a[contains(@href, '&search=')]"));
-            //Assert.AreEqual(0, elements.Count);
-
+            Assert.NotNull(driver.FindElement(By.XPath("//div[@id='content']/p[contains(text(), 'no product')]")));
         }
 
         [Test]
-        public void SearchExTaxItemsTes()
+        public void TooLongSearchTextTest()
         {
-            driver.FindElement(By.Name("search")).SendKeys("%");
+            driver.FindElement(By.Name("search")).SendKeys("sumsungsumsungsumsungsumsungsumsungsumsungsumsungsumsungsumsungsumsungsumsungsumsung");
             driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
 
-            IList<IWebElement> elements = driver.FindElements(By.CssSelector(".price-tax"));
-
-            foreach (IWebElement el in elements)
-            {
-                Assert.IsTrue(el.Text.Contains("Ex Tax: "));
-            }
-
+            Assert.IsFalse((bool)((IJavaScriptExecutor)driver).ExecuteScript("return document.documentElement.scrollWidth>document.documentElement.clientWidth;"));
         }
 
-        [Test]
-        public void Test()
-        {
-            driver.FindElement(By.Name("search")).SendKeys("%");
-            driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
-
-            driver.FindElement(By.XPath("//ul[@class = 'pagination']//a[text() = '>']")).Click();
-        }
     }
 }
