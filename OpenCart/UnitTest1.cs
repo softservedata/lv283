a1f7cf9e47a6d41dc9fb0738f8a4af5f7e6517d3
Vladyslav1995 <vladyslavshynal95@gmail.com>
2018-01-18 10:33:35 +0200

    Add project files.

---
+++ OpenCart/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+//using NSubstitute;
+
+namespace OpenCart
+{
+    //[TestClass]
+    //[TestFixture]
+    public class FunctionalTest
+    {
+        //[TestMethod]
+        //[Test]
+        public void CheckOpen()
+        {
+            Console.WriteLine("Ok");
+        }
+    }
+}
