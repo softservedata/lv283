dc96863769e1c4724b35fee226be497c7c5d598d
Vladyslav1995 <vladyslavshynal95@gmail.com>
2018-01-19 13:47:34 +0200

    Ready for verefication

---
+++ OpenCart/OpenCartTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using OpenQA;
+using NUnit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCart
+{
+    [TestFixture]
+    public class OpenCartTest : RunOpenCartTests
+    {
+        [Test, Order(2)]
+        public void TestDeleteButton()
+        {
+            IWebElement addressBook = driver.FindElement(By.XPath("//a[contains(@href, '/address')]"));
+            addressBook.Click();
+
+            IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id='content']/div[1]/table/tbody/tr[2]/td[2]/a[2]"));
+            deleteButton.Click();
+
+            //Check
+            IWebElement actual = driver.FindElement(By.CssSelector("div>.alert.alert-success"));
+            Assert.AreEqual("Your address has been successfully deleted", actual.Text);
+
+            IWebElement backButton = driver.FindElement(By.CssSelector("a.btn.btn-default"));
+            backButton.Click();
+        }
+
+        [Test, Order(1)]
+        public void TestAddingAditionalAddress()
+        {
+            IWebElement addressBook = driver.FindElement(By.XPath("//a[contains(@href, '/address')]"));
+            addressBook.Click();
+
+            IWebElement newAddressButton = driver.FindElement(By.CssSelector("a.btn.btn-primary"));
+            newAddressButton.Click();
+
+            IWebElement nameTextBox = driver.FindElement(By.Id("input-firstname"));
+            nameTextBox.SendKeys("Marc");
+
+            IWebElement lastnameTextBox = driver.FindElement(By.Id("input-lastname"));
+            lastnameTextBox.SendKeys("Rockfeler");
+
+            IWebElement company = driver.FindElement(By.Id("input-company"));
+            company.SendKeys("Global Development Inc.");
+
+            IWebElement address = driver.FindElement(By.Id("input-address-1"));
+            address.SendKeys("Green Street");
+
+            IWebElement aditionalAddress = driver.FindElement(By.Id("input-address-2"));
+            aditionalAddress.SendKeys("Lindon Johnson Square");
+
+            IWebElement city = driver.FindElement(By.Id("input-city"));
+            city.SendKeys("London");
+
+            IWebElement postcode = driver.FindElement(By.Id("input-postcode"));
+            postcode.SendKeys("88015");
+
+            IWebElement country = driver.FindElement(By.Id("input-country"));
+            country.SendKeys("United Kingdom");
+
+            IWebElement state = driver.FindElement(By.Id("input-zone"));
+            state.SendKeys("Greater London");
+
+            IWebElement continueButton = driver.FindElement(By.CssSelector("input.btn.btn-primary"));
+            continueButton.Click();
+
+            //Check
+            IWebElement actual = driver.FindElement(By.CssSelector("div>.alert.alert-success"));
+            Assert.AreEqual("Your address has been successfully inserted", actual.Text);
+        }
+
+        [Test, Order(3)]
+        public void TestEditAddressButton()
+        {
+            IWebElement addressBook = driver.FindElement(By.XPath("//a[contains(@href, '/address')]"));
+            addressBook.Click();
+
+            IWebElement editButton = driver.FindElement(By.CssSelector("a.btn.btn-info"));
+            editButton.Click();
+
+            IWebElement nameTextBox = driver.FindElement(By.Id("input-firstname"));
+            nameTextBox.Clear();
+            nameTextBox.SendKeys("David");
+
+            IWebElement lastnameTextBox = driver.FindElement(By.Id("input-lastname"));
+            lastnameTextBox.Clear();
+            lastnameTextBox.SendKeys("O`Conner");
+
+            IWebElement address = driver.FindElement(By.Id("input-address-1"));
+            address.Clear();
+            address.SendKeys("Independence Square");
+
+            IWebElement aditionalAddress = driver.FindElement(By.Id("input-address-2"));
+            aditionalAddress.Clear();
+
+            IWebElement city = driver.FindElement(By.Id("input-city"));
+            city.Clear();
+            city.SendKeys("Dublin");
+
+            IWebElement postcode = driver.FindElement(By.Id("input-postcode"));
+            postcode.Clear();
+            postcode.SendKeys("33167");
+
+            IWebElement country = driver.FindElement(By.Id("input-country"));
+            country.SendKeys("Ireland");
+
+            IWebElement state = driver.FindElement(By.Id("input-zone"));
+            state.SendKeys("Dublin");
+
+            IWebElement continueButton = driver.FindElement(By.CssSelector("input.btn.btn-primary"));
+            continueButton.Click();
+
+            //Check
+            IWebElement actual = driver.FindElement(By.CssSelector("div>.alert.alert-success"));
+            Assert.AreEqual("Your address has been successfully updated", actual.Text);
+        }
+
+        [TearDown]
+        public void LoggingOut()
+        {
+            IWebElement logout = driver.FindElement(By.XPath("//a[contains(@href, '/logout')]"));
+        }
+    }
+}
---
+++ OpenCart/RunOpenCartTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using OpenQA;
+using NUnit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCart
+{
+    public abstract class RunOpenCartTests
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void BeforeAllMethods()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            options.AddArguments("--no-proxy-server");
+            options.AddArguments("--ignore-certificate-errors");
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("http://zewer.beget.tech");
+        }
+
+        [TearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void Logging()
+        {
+            IWebElement searchSpan = driver.FindElement(By.ClassName("caret"));
+            searchSpan.Click();
+
+            IWebElement search = driver.FindElement(By.LinkText("Login"));
+            search.Click();
+
+            IWebElement email = driver.FindElement(By.Id("input-email"));
+            email.SendKeys("u1557328@mvrht.net");
+
+            IWebElement password = driver.FindElement(By.Id("input-password"));
+            password.SendKeys("qwerty");
+
+            IWebElement loginButton = driver.FindElement(By.CssSelector("input.btn.btn-primary"));
+            loginButton.Click();
+        }
+    }
+}
