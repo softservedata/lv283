f2446a97b76cfdeca273f936c72d60de253a51cd
Maksym Lukashenko <maksym.lukashenko@gmail.com>
2018-01-19 14:06:54 +0200

    Ready for Verification

--- OpenCartTests/OpenCartTests/UnitTest1.cs
+++ OpenCartTests/OpenCartTests/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace OpenCartTests
     public class UnitTest1 : TestRunner
     {
         // new StreamReader(@"F:\Git\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd().Insert(100, "This is additional string")
-        
+        //Data for tests
         private static readonly object[] ReviewData =
         {
             new object[] { "NotAuthorizedCustomer", "This review created by NotAuthorizedUser" },
@@ -25,9 +25,9 @@ namespace OpenCartTests
         private static readonly object[] ReviewBoundariesValid =
         {
             new object[] { "Has excactly 25 characts." },
-            new object[] { "Has between 25 and 1000 characters" },
             //Can't run tasts with a lot of characters or streamreader
-            new object[] { new StreamReader(@"F:\Git\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd().Remove(0,1)}
+            new object[] { new StreamReader(@"D:\Temp\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd().Remove(0,1)},
+            new object[] { new StreamReader(@"D:\Temp\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd().Remove(0,2)}
         };
         //
         private static readonly object[] ReviewBoundariesInValid =
@@ -35,26 +35,29 @@ namespace OpenCartTests
             new object[] { "Has excactly 24 characts" },
             new object[] { "Has excactly 21 chars" },
             //Can't run tasts with a lot of characters or streamreader
-            new object[] {new StreamReader(@"F:\Git\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd() },
-            new object[] { new StreamReader(@"F:\Git\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd().Insert(0, "Bonus") } };         
+            new object[] {new StreamReader(@"D:\Temp\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd() },
+            new object[] { new StreamReader(@"D:\Temp\OpenCartTests\OpenCartTests\BigReview.txt").ReadToEnd().Insert(0, "Bonus") } };         
         //
-        [Test, TestCaseSource(nameof(ReviewData))]
+        [Test, TestCaseSource(nameof(ReviewData)), Order(1)]
         public void NotLoggedUserReviewCreation(string customerName, string review)
         {
-        
+            //Steps
+            //Getting to review tab
             driver.FindElement(By.XPath(@"//a[text()='Components']")).Click();
             driver.FindElement(By.XPath(@"//a[text() = 'Monitors (2)']")).Click();
             driver.FindElement(By.XPath("//a[contains(@href,'&product_id=42')]")).Click();
             //a[contains(@href,"#tab-review")]
             driver.FindElement(By.XPath("//a[contains(@href,\"#tab-review\")]")).Click();
+            //Creating review with non existing user
             driver.FindElement(By.Id("input-name")).SendKeys(customerName);
             driver.FindElement(By.Id("input-review")).SendKeys(review);
             driver.FindElement(By.CssSelector("input[name=\"rating\"][value =\"3\"]")).Click();
             driver.FindElement(By.Id("button-review")).Click();
-            Thread.Sleep(3000);
+            //Check
+            Assert.IsFalse(driver.FindElement(By.CssSelector(".alert.alert-success")).Enabled);
         }
         //
-        [Test, TestCaseSource(nameof(ReviewBoundariesInValid))]
+        [Test, TestCaseSource(nameof(ReviewBoundariesInValid)), Order(3)]
         public void InvalidReviewLengthCheck(string review)
         {
 
@@ -64,26 +67,33 @@ namespace OpenCartTests
             driver.FindElement(By.Id("input-email")).SendKeys("adelyna.emrie@arockee.com");
             driver.FindElement(By.Id("input-password")).SendKeys("qwerty");
             driver.FindElement(By.Id("input-password")).Submit();
+            //Steps
+            //Getting to review tab
             driver.FindElement(By.XPath(@"//a[text()='Components']")).Click();
             driver.FindElement(By.XPath(@"//a[text() = 'Monitors (2)']")).Click();
             driver.FindElement(By.XPath("//a[contains(@href,'&product_id=42')]")).Click();
             driver.FindElement(By.XPath("//a[contains(@href,\"#tab-review\")]")).Click();
+            //Creating Review
             driver.FindElement(By.Id("input-review")).SendKeys(review);
             driver.FindElement(By.CssSelector("input[name=\"rating\"][value =\"3\"]")).Click();
             driver.FindElement(By.Id("button-review")).Click();
+            //Check
             Assert.IsTrue(driver.FindElement(By.CssSelector(".alert.alert-danger")).Enabled);
+            //Logout
             driver.FindElement(By.CssSelector(".fa.fa-user")).Click();
             driver.FindElement(By.XPath("//div[@id='top-links']//a[contains(@href, 'logout')]")).Click();
         }
         //
-        [Test, TestCaseSource(nameof(ReviewBoundariesValid))]
+        [Test, TestCaseSource(nameof(ReviewBoundariesValid)), Order (2)]
         public void ValidReviewLengthCheck(string review)
         {
+            //Preconditions
             driver.FindElement(By.CssSelector(".fa.fa-user")).Click();
             driver.FindElement(By.XPath("//a[text()='Login']")).Click();
             driver.FindElement(By.Id("input-email")).SendKeys("adelyna.emrie@arockee.com");
             driver.FindElement(By.Id("input-password")).SendKeys("qwerty");
             driver.FindElement(By.Id("input-password")).Submit();
+            //Getting to review tab
             driver.FindElement(By.XPath(@"//a[text()='Components']")).Click();
             driver.FindElement(By.XPath(@"//a[text() = 'Monitors (2)']")).Click();
             driver.FindElement(By.XPath("//a[contains(@href,'&product_id=42')]")).Click();
