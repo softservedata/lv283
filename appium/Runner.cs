12e2d0549dce92371b1b6c3343eee0f31116da45
andriybenesh <andriybenesh@gmail.com>
2018-02-01 17:41:49 +0200

    appium changes

---
+++ appium/Runner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+
+namespace appium
+{
+    [TestFixture]
+    public class TestRunner
+    {
+        protected AppiumDriver<AndroidElement> driver;
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(MobileCapabilityType.App, @"C:\Users\Andrew\AndroidStudioProjects\MyApplication\ApiDemos-debug.apk");
+            capabilities.SetCapability(MobileCapabilityType.DeviceName, "emulator-5554");
+            capabilities.SetCapability(MobileCapabilityType.Udid, "emulator-5554");
+            capabilities.SetCapability(MobileCapabilityType.PlatformVersion, "6.0.0");
+            capabilities.SetCapability(MobileCapabilityType.PlatformName, "Android");
+            capabilities.SetCapability(MobileCapabilityType.FullReset, "false");
+            driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(60));
+        }
+    }
+}
---
+++ appium/Tests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace appium
+{
+    [TestFixture]
+    public class Tests : TestRunner
+    {
+        public void ClickView()
+        {
+            driver.FindElement(By.Id("Views")).Click();
+        }
+
+        [Test, Order(1)]
+        public void PopupMenu()
+        {
+            driver.FindElement(By.Id("Views")).Click();
+            Thread.Sleep(2000);
+            driver.Swipe(750, 1750, 750, 250, 2000);
+            driver.Swipe(650, 1750, 650, 500, 2000);
+            driver.FindElement(By.Id("Popup Menu")).Click();
+            driver.FindElement(By.Id("Make a Popup!")).Click();
+            ReadOnlyCollection<AndroidElement> list = driver.FindElements(By.ClassName("android.widget.TextView"));
+            for (int i = 0; i < 2; i++)
+            {
+                list[i].Click();
+                driver.FindElement(By.Id("Make a Popup!")).Click();
+            }
+            list[2].Click();
+            driver.FindElement(By.ClassName("android.widget.TextView")).Click();
+            Thread.Sleep(2000);
+            driver.Navigate().Back();
+            driver.Navigate().Back();
+        }
+
+
+
+
+        [Test, Order(2)]
+        public void ButtonSmall()
+        {
+            ClickView();
+            driver.FindElement(By.Id("Buttons")).Click();
+            driver.FindElement(By.Id("Small")).Click();
+            driver.Navigate().Back();
+        }
+
+        [Test, Order(3)]
+        public void ButtonNormal()
+        {
+            driver.FindElement(By.Id("Buttons")).Click();
+            driver.FindElement(By.Id("Normal")).Click();
+            driver.Navigate().Back();
+        }
+
+        [Test, Order(4)]
+        public void ChronometerReset()
+        {
+            driver.FindElement(By.Id("Chronometer")).Click();
+            driver.FindElement(By.Id("Reset")).Click();
+            driver.FindElement(By.Id("0 seconds")).Click();
+        }
+
+        [Test, Order(5)]
+        public void ChronometerStartAndStop()
+        {
+            driver.FindElement(By.Id("Start")).Click();
+            driver.FindElement(By.Id("Stop")).Click();
+            driver.FindElement(By.Id("1 seconds")).Click();
+        }
+
+        //[Test, Order(6)]
+        //public void ChronometerStopAndStart()
+        //{
+        //    driver.FindElement(By.Id("Reset")).Click();
+        //    driver.FindElement(By.Id("Start")).Click();
+        //    System.Threading.Thread.Sleep(1000);
+        //    driver.FindElement(By.Id("Stop")).Click();
+        //    System.Threading.Thread.Sleep(1000);
+        //    driver.FindElement(By.Id("Start")).Click();
+        //    driver.FindElement(By.Id("4 seconds")).Click();
+        //}
+
+
+
+
+    }
+}
