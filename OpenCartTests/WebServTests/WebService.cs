d228451b94bcd8742e828753ad4da24143f61325
Maksym Lukashenko <maksym.lukashenko@gmail.com>
2018-02-06 10:45:53 +0200

    Update

--- OpenCartTests/WebServTests/WebService.cs
+++ OpenCartTests/WebServTests/WebService.cs
@@ -10,38 +10,49 @@ namespace WebServTests
     {
         private static readonly object[] TestData =
         {
-            new object[] {0.7, "0.49" },
-            new object[] {-3.3, "10.89" },
-            new object[] {0, "0" },
+            new object[] {0.7},
+            new object[] {-3.3 },
+            new object[] {0}
         };
 
         private static readonly object[] ExceptionData =
         {
-            new object[] { Double.MaxValue,  "NaN, Infinity" },
-            new object[] { Double.MaxValue + 1 , "NaN, Infinity" },
-            new object[] {Double.MaxValue/2 ,"NaN, Infinity"},
-            new object[] { Double.MinValue, "NaN, Infinity" },
-            new object[] { Double.MinValue - 1 , "NaN, Infinity" },
-            new object[] {Double.MinValue/2 , "NaN, Infinity"},
+            new object[] { Double.MaxValue},
+           
+            new object[] {Double.MaxValue/2 },
+            new object[] { Double.MinValue },
+           
+            new object[] {Double.MinValue/2 }
         };
 
         [Test, TestCaseSource(nameof(TestData))]
-        public void TestSqrValid(double argument, string expected)
+        public void TestSqrValid(double argument)
         {
             CalcSEIClient client = new CalcSEIClient();
             Console.WriteLine(client.sqr(argument));
             double actual = client.sqr(argument);
-            Assert.AreEqual(expected, actual.ToString());
+            double expected = client.sqr(argument);
+            Assert.AreEqual(expected, actual);
         }
 
 
         [Test, TestCaseSource(nameof(ExceptionData))]
-        public void TestSqrInvlid(double argument, string expected)
+        public void TestSqrInvlid(double argument)
         {
             CalcSEIClient client = new CalcSEIClient();
+            double actual = 0.0;
+            double expected = 0.0;
             Console.WriteLine(client.sqr(argument));
-            double actual = client.sqr(argument);
-            Assert.IsTrue(expected.Contains(actual.ToString()));
+            try
+            {
+                actual = client.sqr(argument);
+                expected = client.sqr(argument);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Assert.AreEqual(expected, actual);
         }
     }
 
