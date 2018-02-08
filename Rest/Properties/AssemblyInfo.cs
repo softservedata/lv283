e1fc63984f8294a32d410b02a0e255033da26b16
andriybenesh <andriybenesh@gmail.com>
2018-02-08 13:41:29 +0200

    rest

---
+++ Rest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95e4d179-5d62-4da0-9ccf-144d1a372af5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Rest/RestTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.Xml;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Globalization;
+using NUnit.Framework;
+
+
+namespace Rest
+{
+    [DataContract()]
+    public class ResultJsonObj
+    {
+        [DataMember(Name = "origin")]
+        public string Origin { get; set; }
+
+        [DataMember(Name = "url")]
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return "\nOrigin = " + Origin + "\nUrl = " + url;
+        }
+
+    }
+
+    public class RestTest
+    {
+        private ResultJsonObj repositories;
+
+        private async Task FillAll() // GET
+        {
+            string url = "http://httpbin.org/get";
+            string mediaTypeHeaderValue = "application/json";
+            //Dictionary<string, string> requestHeaders = new Dictionary<string, string>();
+            //
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue(mediaTypeHeaderValue));
+            //foreach (string key in requestHeaders.Keys)
+            //{
+            //    client.DefaultRequestHeaders.Add(key, requestHeaders[key]);
+            //}
+
+            //
+            var serializer = new DataContractJsonSerializer(typeof(ResultJsonObj));
+            var streamTask = client.GetStreamAsync(url);
+            //repositories = serializer.ReadObject(await streamTask) as List<ResultJsonObj>;
+            repositories = serializer.ReadObject(await streamTask) as ResultJsonObj;
+        }
+
+
+        public ResultJsonObj GetAll()
+        {
+            FillAll().Wait();
+            return repositories;
+        }
+
+        [Test]
+        public void CheckFoundationExist()
+        {
+            RestTest restTest = new RestTest();
+            ResultJsonObj resultJsonObj = restTest.GetAll();
+            Console.WriteLine("done  Result: " + resultJsonObj);
+        }
+
+    }
+}
