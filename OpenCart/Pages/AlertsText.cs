27288569e817c864980554b3103978b47dcc37f4
Volodymyr <volodymyr482@gmail.com>
2018-02-17 18:10:28 +0200

    add allerts text

---
+++ OpenCart/Pages/AlertsText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart.Pages
+{
+	public class AlertsText
+	{
+		public const string FIRST_NAME_MUST_BE_1_TO_32 = ("First Name must be between 1 and 32 characters!");
+
+		public const string LAST_NAME_MUST_BE_1_TO_32 = ("Last Name must be between 1 and 32 characters!");
+
+		public const string ADDRESS_MUST_BE_3_TO_128 = ("Address must be between 3 and 128 characters!");
+
+		public const string CITY_MUST_BE_2_TO_128 = ("City must be between 2 and 128 characters!");
+
+		public const string NOT_SELECTED_COUNTRY = ("Please select a country!");
+
+		public const string NOT_SELECTED_REGION = ("Please select a region / state!");
+
+		public const string CAN_NOT_DELETE_ADDRESS = ("Warning: You can not delete your default address!");
+
+
+
+
+	}
+}
