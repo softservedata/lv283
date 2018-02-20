f72ccb0b3a81bb35d4a25b36fcb9ff76fde0ebf4
Maksym Lukashenko <maksym.lukashenko@gmail.com>
2018-02-20 19:24:53 +0300

    Add Chosen product page and actions

--- OpenCart/Actions/User/ChosenProductActions.cs
+++ OpenCart/Actions/User/ChosenProductActions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenCart.Pages.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace OpenCart.Actions.User
 {
-    class ChosenProductActions
+    public class ChosenProductActions
     {
+        public ChosenProductPage ChosenProductOperation;
+
+        public ChosenProductActions()
+        {
+            ChosenProductOperation = new ChosenProductPage();
+        }
+        public void GotoReviewTab()
+        {
+            ChosenProductOperation.ClickProductTab("Review");
+        }
+
+        public ChosenProductPage WriteReview(string customerName, string review, int rating)
+        {
+            GotoReviewTab();
+            ChosenProductOperation.ClearNameFieldText();
+            ChosenProductOperation.ClickNameFieldText();
+            ChosenProductOperation.SetNameField(customerName);
+            ChosenProductOperation.ClearReviewFieldText();
+            ChosenProductOperation.ClearReviewFieldText();
+            ChosenProductOperation.SetReviewField(review);
+            ChosenProductOperation.ClickRatingFieldText(rating);
+            ChosenProductOperation.ClickContinueButton();
+            return new ChosenProductPage();
+        }
+
+        public ChosenProductPage WriteReviewAsRegisteredUser(string review, int rating)
+        {
+            GotoReviewTab();
+            ChosenProductOperation.ClearReviewFieldText();
+            ChosenProductOperation.ClearReviewFieldText();
+            ChosenProductOperation.SetReviewField(review);
+            ChosenProductOperation.ClickRatingFieldText(rating);
+            ChosenProductOperation.ClickContinueButton();
+            return new ChosenProductPage();
+        }
     }
 }
--- OpenCart/Pages/User/ChosenProductPage.cs
+++ OpenCart/Pages/User/ChosenProductPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenCart.Tools;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,116 @@ namespace OpenCart.Pages.User
 {
     public class ChosenProductPage
     {
+        private readonly static string NO_SUCH_ELEMENT_FOUND = "No such element found on this page";
+        protected ISearch Search { get; private set; }
+        //		
+        public IWebElement NameField
+        { get { return Search.Id("input-name"); } }
+        public IWebElement ReviewField
+        { get { return Search.Id("input-review"); } }
+
+        public IWebElement ContinueButton
+        { get { return Search.Id("button-review"); } }
+
+        public ICollection<IWebElement> RatingRadioGroup
+        { get { return Search.GetWebElements(By.Name("rating")); } }
+
+        public IWebElement AlertMessage
+        { get { return Search.CssSelector(".alert"); } }
+
+        public ChosenProductPage()
+        {
+            this.Search = Application.Get().Search;
+        }
+
+
+        //Tabs
+        public ICollection<IWebElement> GetProductTabs()
+        {
+
+            return this.Search.CssSelectors(".nav.nav-tabs");
+        }
+
+        public void ClickProductTab(string tabName)
+        {
+            foreach (IWebElement tab in GetProductTabs())
+            {
+                if (tab.Text.Contains(tabName))
+                {
+                    tab.Click();
+                    break;
+                }
+            }
+        }
+
+        //Review
+        //Name
+        public string GetNameFieldText()
+        {
+            return NameField.Text;
+        }
+
+        public void ClearNameFieldText()
+        {
+            NameField.Clear();
+        }
+
+        public void SetNameField(string nameValue)
+        {
+            NameField.SendKeys(nameValue);
+        }
+
+        public void ClickNameFieldText()
+        {
+            NameField.Click();
+        }
+
+        //Review
+        public string GetReviewFieldText()
+        {
+            return ReviewField.Text;
+        }
+
+        public void ClearReviewFieldText()
+        {
+            ReviewField.Clear();
+        }
+
+        public void SetReviewField(string reviewValue)
+        {
+            ReviewField.SendKeys(reviewValue);
+        }
+
+        public void ClickReviewFieldText()
+        {
+            ReviewField.Click();
+        }
+
+        //Rating
+
+        // TODO How?
+        //public string GetRatingFieldText()
+        //{
+        //    return ReviewField.Text;
+        //}
+
+
+
+        public void ClickRatingFieldText(int ratingValue)
+        {
+            Search.CssSelector("input[name=\"rating\"][value =\"3\"]").Click();
+        }
+
+        //ContinueButton
+        public void ClickContinueButton()
+        {
+            ContinueButton.Click();
+        }
+
+        //AlertMessage
+        public string GetMessage()
+        {
+            return AlertMessage.Text;
+        }
     }
 }
