92edce9aa029998df0037f275ca415431403aadc
Kutaev Serhiy <zewer1@yandex.ru>
2018-02-23 00:15:22 +0200

    Added static constructor

--- OpenCart/Pages/User/AHeadComponent.cs
+++ OpenCart/Pages/User/AHeadComponent.cs
@@ -69,7 +69,7 @@ namespace OpenCart.Pages.User
 
     public class MyAccountOptions
     {
-        public static bool IsLoggedin { get; set; } = false;
+        public static bool IsLoggedin { get; set; }// = false;
 
         private ISearch Search { get; set; }
         public IWebElement Register
@@ -81,10 +81,10 @@ namespace OpenCart.Pages.User
         public IWebElement Logout
             { get { return Search.XPath("//a[contains(@href,'route=account/logout')]"); } }
 
-        //static MyAccountOptions()
-        //{
-        //    IsLoggedin = false;
-        //}
+        static MyAccountOptions()
+        {
+            IsLoggedin = false;
+        }
 
         public MyAccountOptions()
         {
