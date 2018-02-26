1d1ef64cd4e41d71fbaeeb5326a63788027383ad
Maksym Lukashenko <maksym.lukashenko@gmail.com>
2018-02-27 00:00:31 +0300

    Add admin pages

---
+++ OpenCart/Pages/Admin/AdminReviewsPage.cs
@@ -0,0 +1,55 @@
+﻿using OpenCart.Tools;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart.Pages.Admin
+{
+    class AdminReviewsPage
+    {
+        private readonly string DATE_FORMAT = "{0}-{1}-{2}";
+        private ISearch Search;
+
+        public IWebElement Product
+        { get { return Search.Id("input-product"); } }
+
+        public IWebElement Author
+        { get { return Search.Id("input-author"); } }
+
+        public IWebElement Status
+        { get { return Search.Id("input-status"); } }
+
+        public IWebElement DateAdded
+        { get {return Search.ClassName("input-group.date"); } }
+
+        public IWebElement FilterButton
+        { get { return Search.Id("button-filter"); } }
+
+
+        //DateAdded
+        public void ClearDateAdded()
+        {
+            DateAdded.Clear();
+        }
+
+        public void SetDateAdded(DateTime date)
+        {
+            DateAdded.SendKeys(String.Format(DATE_FORMAT,date.Year.ToString(), date.Month.ToString(), date.Day.ToString()));
+        }
+
+
+        //FilterButton
+        public void ClickFilterButton()
+        {
+            FilterButton.Click();
+        }
+
+        public IWebElement FindTableFirstRow()
+        {
+            return Search.XPath("//tbody/tr[text()]");
+        }
+    }
+}
